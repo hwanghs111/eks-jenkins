f195936c557de387767f675fdb02ce21e580bb75
Hyunsu Hwang <65887370+hwanghs111@users.noreply.github.com>
2024-04-22 15:12:21 +0900

    Update HelloworldApplication.java

--- src/main/java/org/example/helloworld/HelloworldApplication.java
+++ src/main/java/org/example/helloworld/HelloworldApplication.java
@@ -11,7 +11,7 @@ public class HelloworldApplication {
 
 	@GetMapping("/")
 	public String hello(){
-		return "hello world";
+		return "hello thumbnails";
 	}
 
 	public static void main(String[] args) {
